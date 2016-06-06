22a4cf1f9dafb280e7750f9195c035cb0241ddaf
jcanderson195 <jcanderson195@gmail.com>
2016-06-06 18:14:23 -0500

    beginning stages of website design

---
+++ NoRegretsWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NoRegretsWebsite.Startup))]
+namespace NoRegretsWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
